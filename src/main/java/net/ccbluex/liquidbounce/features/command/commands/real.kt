90de61bc8837cd2f1651c61e9847991d223a9770
Falcon249 <131998767+Falcon249@users.noreply.github.com>
2025-07-15 16:27:17 +0200

    test command

---
+++ src/main/java/net/ccbluex/liquidbounce/features/command/commands/real.kt
@@ -0,0 +1,23 @@
+package net.ccbluex.liquidbounce.features.command.commands
+
+import net.ccbluex.liquidbounce.features.command.Command
+
+object VClipCommand : Command("fent") {
+    /**
+     * Execute commands with provided [args]
+     */
+    override fun execute(args: Array<String>) {
+        if (args.size > 1) {
+            try {
+                args[1].toDouble()
+                chat("fent ${args[1]}")
+            } catch (ex: NumberFormatException) {
+                chatSyntaxError()
+            }
+
+            return
+        }
+
+        chatSyntax("fent <value>")
+    }
+}
